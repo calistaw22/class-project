3c86dd0999b58400178878821a28adb213ff6633
calistaw22 <91386468+calistaw22@users.noreply.github.com>
2021-09-26 03:39:18 -0500

    Create draft.py

---
+++ draft.py
@@ -0,0 +1,9 @@
+# Get information
+city=input("Please enter city: ")
+zipcode=input("Please enter zipcode: ")
+#Request data from webservice --> http://openweathermap.org/
+#Use try blocks to establish the connection to webservice
+if weather valid== 'successful'
+if not =='try again'
+#Display weather if successful
+#Create loop to have user run program again.
